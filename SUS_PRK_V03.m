ecf32f69318cfa4c55ffc9889cde9ce5331eb784
onzeq <onzeq.v@googlemail.com>
2020-07-10 08:53:15 +0200

    ...

--- SUS_PRK_V03.m
+++ SUS_PRK_V03.m
@@ -1,1 +1,13 @@
 %Signale Systeme1 Praktikum Nr 3 Jan Richert, Christian Walz, Jakob Roll 
+
+clc;
+clear;
+close all;
+clear variables;
+
+%% Wichtigste Parameter
+
+fs=50000;           % Abtastrate 
+simdur=1;           % Zeitspanne, die simuliert werden soll (1s)
+ts=1/fs;            % Abtastperiode
+Nsamps=simdur/ts;   % Anzahl Abtastwerte, die erzeugt werden
