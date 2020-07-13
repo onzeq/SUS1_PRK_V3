0ba38d31ad2966247a43ccfa8147ffbdb6345de9
Christian <Christian@192.168.0.171>
2020-07-13 11:06:25 +0200

    regelung²

--- SUS_PRK_V03.m
+++ SUS_PRK_V03.m
@@ -5,287 +5,68 @@ clear variables;
 
 %% Wichtigste Parameter
 % Wichtig: innerhalb einer Simulationskette nach Möglichkeit nur eine
-% Abtastrate verwenden!!!!!
-fs=81920;           % Abtastrate 
-f0=800;             % Grundfrequenz 
-f1=900;             % Grundfrequenz 
-f2=1600;            % Grundfrequenz 
-f3=2000;            % Grundfrequenz 
-f4=3200;            % Grundfrequenz 
+% 'leere' Signalstruktur als Vorlage für andere Signale erzeugen
 
-
+%%----------------------------------------------------------------------------------------------------------------
+%%Aufgabe1
+%%Allgemeine vorgabe T=10ms
+fs = 50000;
+Ts = 1/fs;
+tvec = -0.5: Ts : 0.5;
+tvec = tvec(1:50000);
 
 simdur=1;           % Zeitspanne, die simuliert werden soll (1s)
 ts=1/fs;            % Abtastperiode
 Nsamps=simdur/ts;   % Anzahl Abtastwerte, die erzeugt werden
 
-%% Sinus-Signale
-sig0=create_sin_rect(fs,f0,-1,Nsamps);
-sig1=create_sin_rect(fs,f1,-1,Nsamps);
-sig2=create_sin_rect(fs,f2,-1,Nsamps);
-sig3=create_sin_rect(fs,f3,-1,Nsamps);
+%%Signale erzeugen
+%%signala
+siga=square(2*pi*500*tvec);
+%%plot(tvec,siga);
 
-% 'leere' Signalstruktur als Vorlage für andere Signale erzeugen
-sig_template.tsample=sig0.tsample;
-sig_template.tstart=sig0.tstart;
+%%signalb
+sigb= create_cos_rect(fs,500,-1,Nsamps);
+sig_template.tsample=sigb.tsample;
+sig_template.tstart=sigb.tstart;
+%%plot(tvec,sigb.samples);
 
-% zwei 'Rauschsignale' erzeugen
-sig4=sig_template;
-sig4.samples=randn(size(sig0.samples));
-sig5=sig_template;
-sig5.samples=randn(size(sig0.samples));
+%%signalc
+sigc= create_cos_rect(fs,2000,-1,Nsamps);
+%%plot(tvec,sigc.samples);
 
-%%----------------------------------------------------------------------------------------------------------------
-%%Aufgabe1
-%%Allgemeine vorgabe T=10ms
-T=0.01;
+%%signald
+sigd = create_sinc_rect(fs,125,-1,Nsamps);
+%%plot(tvec, sigd.samples);
 
-%%signal1
-%Signalspezifische Variablen -->n muss nur in plus und minus interschieden werden!
-Tp=T; %10ms Periodendauer
-Ts=20*1e-6; %Abtastfrequenz
+%%Systeme erzeugen
+nullvec = -0.5: Ts : 0.5;
+nullvec = nullvec(1:50000);
+nullvec = 0*nullvec;
 
-%Ans für alle Fälle
-A_plus_signal1=0.5;
-A_minus_signal1=0.5;
+%%Systema --> dirac mit 1 bei 0 und 1 ms
+systema = nullvec;
+systema(25001) = 1;
+systema(25051) = 1;
+%plot(tvec,systema);
 
-%Zeitvektor
-tvec = -50/1000: Ts : 50/1000;
-vec=0*tvec; %ver tvec zuweisen = 0
+%%Systema --> dirac mit 1 bei 0 und 2 ms
+systemb = nullvec;
+systemb(25001) = 1;
+systemb(25101) = 1;
+%plot(tvec,systemb);
 
-%Summenfunktion
-vec=vec+A_plus_signal1*exp(1i*2*pi*1/Tp*tvec);
-vec=vec+A_minus_signal1*exp(1i*2*pi*1/Tp*-1*tvec);   
-
-%Ergebnis FourierSynthese plotten
-figure();
-plot(tvec, vec);
-dodo=axis;
-dodo(1)=-0.05; dodo(2)=0.05;
-dodo(3)=-1.1; dodo(4)=1.1;
-axis(dodo);
-title('sig1(t) nach Fourier-Synthese');
-xlabel('t in s');
-ylabel('Amplitude');
-
-%Frequenzspektrum plotten
-vec1.samples = vec;
-vec1.tsample= Ts;
-plot_signal_spectrum(vec1);
-
-%OrginalSignal
-sig1 = cos(2*pi*tvec/T);
-plot(tvec,sig1);
-title('sig1(t) OrginalSignal');
-xlabel('t in s');
-ylabel('Amplitude');
-
-%-------------------------------------------------------------------------------------------------
-%%signal2
-%Signalspezifische Variablen -->n muss nur in plus und minus interschieden werden!
-Tp=3*T; %Periodendauer 30ms
-Ts=20*1e-6; %Abtastfrequenz
-
-%Ans für alle Fälle
-A_plus_signal2=0.5;
-A_minus_signal2=0.5;
-
-%Zeitvektor
-tvec = -50/1000: Ts : 50/1000;
-vec=0*tvec; %ver tvec zuweisen = 0
-
-%Summenfunktion
-vec=vec+A_plus_signal2*exp(1i*2*pi*1/Tp*tvec);
-vec=vec+A_minus_signal2*exp(1i*2*pi*1/Tp*-1*tvec);   
-
-%Ergebnis FourierSynthese plotten
-figure();
-plot(tvec, vec);
-dodo=axis;
-dodo(1)=-0.05; dodo(2)=0.05;
-dodo(3)=-1.1; dodo(4)=1.1;
-axis(dodo);
-title('sig2(t) nach Fourier-Synthese');
-xlabel('t in s');
-ylabel('Amplitude');
-
-%Frequenzspektrum plotten
-vec2.samples = vec;
-vec2.tsample= Ts;
-plot_signal_spectrum(vec2);
-
-%OrginalSignal
-sig2 = cos(2*pi*tvec/(3*T));
-plot(tvec,sig2);
-title('sig2(t) Frequenzspektrum');
-xlabel('t in s');
-ylabel('Amplitude');
-
-%-------------------------------------------------------------------------------------------------
-%%signal3
-%Signalspezifische Variablen
-Tp=20/1000; 
-Ts=20*1e-6; 
-
-%verschiedene ns
-n=50; %n je nach graph anpassen
-nvec=-n:1:n;
-nvec_eve_signal3=-n:2:n;
-nvec_odd_signal3=-n+1:2:n-1;
-
-%Ans für alle Fälle
-An_null_signal3=0.5;
-An_even_signal3=0;
-An_odd_signal3=(1./(pi.*nvec_odd_signal3)).*((-1).^((nvec_odd_signal3-1)/2));
-
-%Zeitvektor
-tvec = -50/1000: Ts : 50/1000;
-vec=0*tvec;
-
-%Summenfunktion
-for it=1:length(nvec_odd_signal3)
-    n=nvec_odd_signal3(it);
-    An=An_odd_signal3(it);
-    vec=vec+An*exp(1i*2*pi*f0*n*tvec);       
+%%Systemc --> rect T=2ms
+systemc = nullvec;
+systemc(24951) = 1;
+systemc(25051) = 1;
+for it=24951:25051
+    systemc(it)=1;       
 end
-%An_null nicht vergessen
-vec=vec+An_null_signal3;
+%plot(tvec, systemc);
 
-%Ergebnis FourierSynthese plotten
-figure();
-plot(2*tvec/Tp, vec);
-title('sig3(t) nach Fourier-Synthese');
-dodo=axis;
-dodo(1)=-0.05; dodo(2)=0.05;
-dodo(3)=-0.1; dodo(4)=1.1;
-axis(dodo);
-xlabel('t in s');
-ylabel('Amplitude');
-
-%Frequenzspektrum plotten
-vec3.samples = vec;
-vec3.tsample= Ts;
-plot_signal_spectrum(vec3);
-
-%OrginalSignal
-figure();
-t=-4*pi:ts*100:4*pi;
-sig3=0.5*square(t+0.5*pi)+0.5;
-plot(t/pi,sig3);
-title('sig3 OrignalFunktion');
-dodo=axis;
-dodo(1)=-4; dodo(2)=4;
-dodo(3)=-0.1; dodo(4)=1.1;
-axis(dodo);
-xlabel('t in T');
-ylabel('Amplitude');
-
-%-------------------------------------------------------------------------------------------------
-%%signal4
-%Signalspezifische Variablen
-Tp=20/1000; 
-Ts=20*1e-6; 
-
-%verschiedene ns
-n=50; %n je nach graph anpassen
-nvec=-n:1:n;
-nvec_eve_signal4=-n:2:n;
-nvec_odd_signal4=-n+1:2:n-1;
-
-%Ans für alle Fälle
-An_null_signal4=0.5;
-An_even_signal4=0;
-An_odd_signal4=-(1i./(pi.*nvec_odd_signal4));
-
-%Zeitvektor
-tvec = -50/1000: Ts : 50/1000;
-vec=0*tvec;
-
-%Summenfunktion
-for it=1:length(nvec_odd_signal4)
-    n=nvec_odd_signal4(it);
-    An=An_odd_signal4(it);
-    vec=vec+An*exp(1i*2*pi*f0*n*tvec);       
-end
-%An_null nicht vergessen
-vec=vec+An_null_signal4;
-
-%Ergebnis FourierSynthese plotten
-figure();
-plot(2*tvec/Tp, vec);
-title('sig4(t) nach Fourier-Synthese');
-dodo=axis;
-dodo(1)=-0.05; dodo(2)=0.15;
-dodo(3)=-0.1; dodo(4)=1.1;
-axis(dodo);
-xlabel('t in s');
-ylabel('Amplitude');
-
-%Frequenzspektrum plotten
-vec4.samples = vec;
-vec4.tsample= Ts;
-plot_signal_spectrum(vec4);
-
-%OrginalSignal
-figure();
-t=-4*pi:ts*100:4*pi;
-sig4=0.5*square(t)+0.5;
-plot(t/pi,sig4);
-title('sig4 OrignalFunktion');
-dodo=axis;
-dodo(1)=-4; dodo(2)=4;
-dodo(3)=-0.1; dodo(4)=1.1;
-axis(dodo);
-xlabel('t in T');
-ylabel('Amplitude');
-
-%-------------------------------------------------------------------------------------------------
-%%Aufgabe2
-%Die beiden Rechtecksignale bereits in Aufgabe 1 angelegt:
-%vec 3 spektrum
-vec3_2.samples = sig3;
-vec3_2.tsample= Ts;
-plot_signal_spectrum(vec3_2);
-%vec4 spektrum
-vec4_2.samples = sig4;
-vec4_2.tsample= Ts;
-plot_signal_spectrum(vec4_2);
-
-
-%Sinus-Signal mit Frequenz 500Hz wird erzeugt.
-signal2b=create_sin_rect(fs,500,-1,Nsamps);
-
-%Signal 2b Zeitsignal wird geplottet
-plot_timesig(signal2b);
-title('signal 2b)');
-dodo=axis;
-dodo(1)=0; dodo(2)=0.01;
-dodo(3)=-1.1; dodo(4)=1.1;
-axis(dodo);
-%Signalspektrum:
-plot_signal_spectrum(signal2b);
-
-%gefenstertes Sinus-Signal mit Frequenz 500Hz wird erzeugt.
-signal2c=create_sin_rect(fs,500,20,Nsamps);
-%Signal 2c Zeitsignal wird geplottet
-plot_timesig(signal2c);
-title('signal 2c)');
-dodo=axis;
-dodo(1)=-0.03; dodo(2)=0.03;
-dodo(3)=-1.1; dodo(4)=1.1;
-axis(dodo);
-%Signalspektrum:
-plot_signal_spectrum(signal2c);
-
-%%d) sinc signal erzeugen und plotten
-signal2d=create_sinc_rect(fs,125,-1,Nsamps);
-plot_timesig(signal2d)
-title('signal 2d)')
-dodo=axis;
-dodo(1)=-0.5; dodo(2)=0.5;
-axis(dodo);
-%Signalspektrum:
-plot_signal_spectrum(signal2d);
+%%Systemd --> sinc mit T = 2ms
+systemd = create_sinc_rect(fs,500,-1,Nsamps);
+plot(tvec, systemd.samples);
 
 %-------------------------------------------------------------------------------
 %% Funktionen 
